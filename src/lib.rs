ea15bb70234d44a7844ce0460ab70eae77cf9f29
Fernando A. Fraticelli <53105809+mrtelecaster@users.noreply.github.com>
2023-01-13 12:42:54 -0500

    refactor: revert to logic used in train proto

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,28 @@
 //! Used to define a curve that you can interpolate smoothly from the start to end position
 
 
-use std::ops::Div;
+use bevy::prelude::Vec3;
 
+mod line;
+
+#[cfg(test)]
 mod vec;
 
 
 /// A curve that can be smoothly interpolated over
-pub trait Curve<T> where T: Div<Output=T> + PartialEq {
+pub trait Curve {
 
 	/// Samples the curve at the given fractional position, with `0.0` being the start of the curve
 	/// and `1.0` being the end. Returns a tuple with the position and tangent vector of the 
-	fn sample(&self, t: T) -> (vec::Vec3<T>, vec::Vec3<T>);
+	fn sample(&self, t: f32) -> (Vec3, Vec3);
 
 	/// Gets the length of the path traced by this curve in world units
-	fn real_length(&self) -> T;
+	fn real_length(&self) -> f32;
 
 	/// Samples the curve at the given point by real distance along the curve
 	///
 	/// The returned value contains the position and tangent vector
-	fn sample_dist(&self, dist: T) -> (vec::Vec3<T>, vec::Vec3<T>) {
+	fn sample_dist(&self, dist: f32) -> (Vec3, Vec3) {
 		self.sample(dist / self.real_length())
 	}
 }
--- src/vec.rs
+++ src/vec.rs
@@ -1,95 +1,222 @@
+use bevy::prelude::Vec3;
 use approx::{AbsDiffEq, RelativeEq, UlpsEq};
 
 
+/// Wrapper type around a [Bevy engine `Vec3`](Vec3) that supports approximate comparison using
+/// [the `approx` crate](approx)
+/// 
+/// Mostly used for unit testing
+#[derive(Debug, PartialEq)]
+pub struct ApproxVec { vec: Vec3 }
 
-/// A position in 3D space
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub struct Vec3<T> where T: PartialEq {
-	/// X value of this vector
-	pub x: T,
-	/// Y value of this vector
-	pub y: T,
-	/// Z value of this vector
-	pub z: T,
-}
+impl ApproxVec {
 
-impl<T> Vec3<T> where T: PartialEq {
-	/// 
-	pub fn new(x: T, y: T, z: T) -> Self {
-		Self{ x, y, z }
+	/// Creates a new vector with the given X, Y, and Z values
+	#[inline]
+	pub fn new(x: f32, y: f32, z: f32) -> Self {
+		Self{ vec: Vec3::new(x, y, z) }
 	}
 }
 
-impl<T> AbsDiffEq for Vec3<T> where T: AbsDiffEq<Epsilon=T> + Clone + Copy + PartialEq {
+impl From<Vec3> for ApproxVec {
+    fn from(vec: Vec3) -> Self {
+        Self{ vec }
+    }
+}
 
-    type Epsilon = T;
+impl From<&Vec3> for ApproxVec {
+    fn from(v: &Vec3) -> Self {
+        Self{ vec: v.clone() }
+    }
+}
+
+impl AbsDiffEq for ApproxVec {
+
+    type Epsilon = f32;
 
     fn default_epsilon() -> Self::Epsilon {
-        T::default_epsilon()
+        f32::default_epsilon()
     }
 
     fn abs_diff_eq(&self, other: &Self, epsilon: Self::Epsilon) -> bool {
-        self.x.abs_diff_eq(&other.x, epsilon)
-		&& self.y.abs_diff_eq(&other.y, epsilon)
-		&& self.z.abs_diff_eq(&other.z, epsilon)
+        self.vec.x.abs_diff_eq(&other.vec.x, epsilon)
+		&& self.vec.y.abs_diff_eq(&other.vec.y, epsilon)
+		&& self.vec.z.abs_diff_eq(&other.vec.z, epsilon)
     }
 }
 
-impl<T> RelativeEq for Vec3<T> where T: AbsDiffEq<Epsilon=T> + Clone + Copy + RelativeEq {
+impl RelativeEq for ApproxVec {
+
     fn default_max_relative() -> Self::Epsilon {
-        T::default_max_relative()
+        f32::default_max_relative()
     }
 
     fn relative_eq(&self, other: &Self, epsilon: Self::Epsilon, max_relative: Self::Epsilon) -> bool {
-        self.x.relative_eq(&other.x, epsilon, max_relative)
-		&& self.y.relative_eq(&other.y, epsilon, max_relative)
-		&& self.z.relative_eq(&other.z, epsilon, max_relative)
+        self.vec.x.relative_eq(&other.vec.x, epsilon, max_relative)
+		&& self.vec.y.relative_eq(&other.vec.y, epsilon, max_relative)
+		&& self.vec.z.relative_eq(&other.vec.z, epsilon, max_relative)
     }
 }
 
-impl<T> UlpsEq for Vec3<T> where T: AbsDiffEq<Epsilon=T> + Clone + Copy + UlpsEq {
+impl UlpsEq for ApproxVec {
+
     fn default_max_ulps() -> u32 {
-        T::default_max_ulps()
+        f32::default_max_ulps()
     }
 
     fn ulps_eq(&self, other: &Self, epsilon: Self::Epsilon, max_ulps: u32) -> bool {
-        self.x.ulps_eq(&other.x, epsilon, max_ulps)
-		&& self.y.ulps_eq(&other.y, epsilon, max_ulps)
-		&& self.z.ulps_eq(&other.z, epsilon, max_ulps)
+        self.vec.x.ulps_eq(&other.vec.x, epsilon, max_ulps)
+		&& self.vec.y.ulps_eq(&other.vec.y, epsilon, max_ulps)
+		&& self.vec.z.ulps_eq(&other.vec.z, epsilon, max_ulps)
     }
 }
-
 
 
 #[cfg(test)]
 mod tests {
 
-	use super::*;
+	mod traits {
 
-	mod approx_impl {
+		mod abs_diff_eq {
 
-		use super::*;
-		use approx::{assert_abs_diff_eq, assert_relative_eq, assert_ulps_eq};
+			use crate::vec::ApproxVec;
+			use approx::{assert_abs_diff_eq, assert_abs_diff_ne};
 
-		#[test]
-		pub fn abs_diff_eq() {
-			let a = Vec3::new(1.0, -2.0, 3.0);
-			let b = Vec3::new(1.0, -2.0, 3.0);
-			assert_abs_diff_eq!(a, b);
+			/// Basic test case demonstrating intended usage
+			#[test]
+			fn basic() {
+				assert_abs_diff_eq!(ApproxVec::new(0.0, 0.0, 0.0), ApproxVec::new(0.0, 0.0, 0.0));
+				assert_abs_diff_eq!(ApproxVec::new(1.0, 2.0, 3.0), ApproxVec::new(1.0, 2.0, 3.0));
+				assert_abs_diff_eq!(ApproxVec::new(-1.0, 2.0, -3.0), ApproxVec::new(-1.0, 2.0, -3.0));
+				assert_abs_diff_eq!(ApproxVec::new(1.0, -2.0, 3.0), ApproxVec::new(1.0, -2.0, 3.0));
+
+				assert_abs_diff_ne!(ApproxVec::new(1.0, 2.0, 3.0), ApproxVec::new(2.0, 3.0, 4.0));
+				assert_abs_diff_ne!(ApproxVec::new(1.0, 2.0, 3.0), ApproxVec::new(0.0, 2.0, 3.0));
+				assert_abs_diff_ne!(ApproxVec::new(1.0, 2.0, 3.0), ApproxVec::new(1.0, 0.0, 3.0));
+				assert_abs_diff_ne!(ApproxVec::new(1.0, 2.0, 3.0), ApproxVec::new(1.0, 2.0, 0.0));
+			}
+
+			/// Check equality of values that are close to zero without being approximately equal
+			#[test]
+			fn zero() {
+				assert_abs_diff_eq!(ApproxVec::new(0.0, 0.0, 0.0), ApproxVec::new(0.0, 0.0, 0.0));
+				assert_abs_diff_eq!(ApproxVec::new(-0.0, 0.0, -0.0), ApproxVec::new(0.0, -0.0, -0.0));
+
+				assert_abs_diff_ne!(ApproxVec::new(0.000001, 0.0, 0.0), ApproxVec::new(0.0, 0.0, 0.0));
+				assert_abs_diff_ne!(ApproxVec::new(0.0, 0.0, 0.0), ApproxVec::new(0.0, 0.000001, 0.0));
+				assert_abs_diff_ne!(ApproxVec::new(0.0, 0.0, -0.000001), ApproxVec::new(0.0, 0.0, 0.0));
+				assert_abs_diff_ne!(ApproxVec::new(0.0, 0.0, 0.0), ApproxVec::new(-0.000001, 0.0, 0.0));
+			}
+
+			/// Test approximate equality of large numbers
+			#[test]
+			fn big() {
+				assert_abs_diff_eq!(ApproxVec::new(100000000.0, 0.0, 0.0), ApproxVec::new(100000001.0, 0.0, 0.0));
+				assert_abs_diff_eq!(ApproxVec::new(0.0, 100000001.0, 0.0), ApproxVec::new(0.0, 100000000.0, 0.0));
+				assert_abs_diff_eq!(ApproxVec::new(0.0, 0.0, -100000000.0), ApproxVec::new(0.0, 0.0, -100000001.0));
+				assert_abs_diff_eq!(ApproxVec::new(-100000001.0, 0.0, 0.0), ApproxVec::new(-100000000.0, 0.0, 0.0));
+
+				assert_abs_diff_ne!(ApproxVec::new(0.0, 10000.0, 0.0), ApproxVec::new(0.0, 10001.0, 0.0));
+				assert_abs_diff_ne!(ApproxVec::new(0.0, 0.0, 10001.0), ApproxVec::new(0.0, 0.0, 10000.0));
+				assert_abs_diff_ne!(ApproxVec::new(-10000.0, 0.0, 0.0), ApproxVec::new(-10001.0, 0.0, 0.0));
+				assert_abs_diff_ne!(ApproxVec::new(0.0, -10001.0, 0.0), ApproxVec::new(0.0, -10000.0, 0.0));
+			}
+
+			/// Test approximate equality of numbers around 1
+			#[test]
+			fn med() {
+				assert_abs_diff_eq!(ApproxVec::new(1.0000001, 0.0, 0.0), ApproxVec::new(1.0000002, 0.0, 0.0));
+				assert_abs_diff_eq!(ApproxVec::new(0.0, 1.0000002, 0.0), ApproxVec::new(0.0, 1.0000001, 0.0));
+				assert_abs_diff_eq!(ApproxVec::new(0.0, 0.0, -1.0000001), ApproxVec::new(0.0, 0.0, -1.0000002));
+				assert_abs_diff_eq!(ApproxVec::new(-1.0000002, 0.0, 0.0), ApproxVec::new(-1.0000001, 0.0, 0.0));
+
+				assert_abs_diff_ne!(ApproxVec::new(0.0, 1.000001, 0.0), ApproxVec::new(0.0, 1.000002, 0.0));
+				assert_abs_diff_ne!(ApproxVec::new(0.0, 0.0, 1.000002), ApproxVec::new(0.0, 0.0, 1.000001));
+				assert_abs_diff_ne!(ApproxVec::new(-1.000001, 0.0, 0.0), ApproxVec::new(-1.000002, 0.0, 0.0));
+				assert_abs_diff_ne!(ApproxVec::new(0.0, -1.000002, 0.0), ApproxVec::new(0.0, -1.000001, 0.0));
+			}
+
+			/// Test approximate equality of small numbers close to zero
+			#[test]
+			fn small() {
+				assert_abs_diff_eq!(ApproxVec::new(0.0, 0.0,0.000010001), ApproxVec::new(0.0, 0.0, 0.000010002));
+				assert_abs_diff_eq!(ApproxVec::new(0.000010002, 0.0, 0.0), ApproxVec::new(0.000010001, 0.0, 0.0));
+				assert_abs_diff_eq!(ApproxVec::new(0.0, -0.000010001, 0.0), ApproxVec::new(0.0, -0.000010002, 0.0));
+				assert_abs_diff_eq!(ApproxVec::new(0.0, 0.0, -0.000010002), ApproxVec::new(0.0, 0.0, -0.000010001));
+
+				assert_abs_diff_ne!(ApproxVec::new(0.000001002, 0.0, 0.0), ApproxVec::new(0.0000001001, 0.0, 0.0));
+				assert_abs_diff_ne!(ApproxVec::new(0.0, 0.000001001, 0.0), ApproxVec::new(0.0, 0.0000001002, 0.0));
+				assert_abs_diff_ne!(ApproxVec::new(0.0, 0.0, -0.000001002), ApproxVec::new(0.0, 0.0, -0.0000001001));
+				assert_abs_diff_ne!(ApproxVec::new(-0.000001001, 0.0, 0.0), ApproxVec::new(-0.0000001002, 0.0, 0.0));
+			}
 		}
 
-		#[test]
-		pub fn relative_eq() {
-			let a = Vec3::new(1.0, -2.0, 3.0);
-			let b = Vec3::new(1.0, -2.0, 3.0);
-			assert_relative_eq!(a, b);
+		mod relative_eq {
+
+			use approx::{assert_relative_eq, assert_relative_ne};
+			use crate::vec::ApproxVec;
+
+			/// Demonstrate typical usage of [`ApproxVec::relative_eq`]
+			#[test]
+			fn basic() {
+				assert_relative_eq!(ApproxVec::new(1.0, 2.0, 3.0), ApproxVec::new(1.0, 2.0, 3.0));
+				assert_relative_eq!(ApproxVec::new(1.0, 2.0, 3.0), ApproxVec::new(1.0000001, 2.0000001, 3.0000001));
+
+				assert_relative_ne!(ApproxVec::new(1.0, 2.0, 3.0), ApproxVec::new(1.000001, 2.000001, 3.000001));
+			}
+
+			/// Test values near zero
+			#[test]
+			fn zero() {
+				assert_relative_eq!(ApproxVec::new(0.0, 0.0, 0.0), ApproxVec::new(0.0, 0.0, 0.0));
+				assert_relative_eq!(ApproxVec::new(0.0, 0.0, 0.0), ApproxVec::new(0.0, -0.0, 0.0));
+				assert_relative_eq!(ApproxVec::new(0.0, 0.0, -0.0), ApproxVec::new(0.0, 0.0, 0.0));
+				assert_relative_eq!(ApproxVec::new(-0.0, 0.0, 0.0), ApproxVec::new(-0.0, 0.0, 0.0));
+
+				assert_relative_ne!(ApproxVec::new(0.0, 0.000001, 0.0), ApproxVec::new(0.0, 0.0, 0.0));
+				assert_relative_ne!(ApproxVec::new(0.0, 0.0, 0.0), ApproxVec::new(0.0, 0.0, 0.000001));
+				assert_relative_ne!(ApproxVec::new(-0.000001, 0.0, 0.0), ApproxVec::new(0.0, 0.0, 0.0));
+				assert_relative_ne!(ApproxVec::new(0.0, 0.0, 0.0), ApproxVec::new(0.0, -0.000001, 0.0));
+			}
 		}
 
-		#[test]
-		pub fn ulps_eq() {
-			let a = Vec3::new(1.0, -2.0, 3.0);
-			let b = Vec3::new(1.0, -2.0, 3.0);
-			assert_ulps_eq!(a, b);
+		mod ulps_eq {
+
+			use approx::{assert_ulps_eq, assert_ulps_ne};
+			use crate::vec::ApproxVec;
+
+			/// Demonstrate typical usage of [`ApproxVec::ulps_eq`] in unit test assertions
+			#[test]
+			fn basic() {
+				assert_ulps_eq!(ApproxVec::new(1.0, 2.0, 3.0), ApproxVec::new(1.0, 2.0, 3.0));
+				assert_ulps_eq!(ApproxVec::new(1.0, 2.0, 3.0), ApproxVec::new(1.0000001, 2.0000001, 3.0000001));
+
+				assert_ulps_ne!(ApproxVec::new(1.0, 2.0, 3.0), ApproxVec::new(1.000001, 2.000001, 3.000001));
+			}
+
+			/// Test values near zero
+			#[test]
+			fn zero() {
+				assert_ulps_eq!(ApproxVec::new(0.0, 0.0, 0.0), ApproxVec::new(0.0, 0.0, 0.0));
+				assert_ulps_eq!(ApproxVec::new(0.0, 0.0, 0.0), ApproxVec::new(0.0, -0.0, 0.0));
+				assert_ulps_eq!(ApproxVec::new(0.0, 0.0, -0.0), ApproxVec::new(0.0, 0.0, 0.0));
+				assert_ulps_eq!(ApproxVec::new(-0.0, 0.0, 0.0), ApproxVec::new(-0.0, 0.0, 0.0));
+
+				assert_ulps_ne!(ApproxVec::new(0.0, 0.000001, 0.0), ApproxVec::new(0.0, 0.0, 0.0));
+				assert_ulps_ne!(ApproxVec::new(0.0, 0.0, 0.0), ApproxVec::new(0.0, 0.0, 0.000001));
+				assert_ulps_ne!(ApproxVec::new(-0.000001, 0.0, 0.0), ApproxVec::new(0.0, 0.0, 0.0));
+				assert_ulps_ne!(ApproxVec::new(0.0, 0.0, 0.0), ApproxVec::new(0.0, -0.000001, 0.0));
+			}
+
+			#[test]
+			fn infinity() {
+				assert_ulps_eq!(ApproxVec::new(f32::INFINITY, 0.0, 0.0), ApproxVec::new(f32::INFINITY, 0.0, 0.0));
+				assert_ulps_eq!(ApproxVec::new(0.0, f32::NEG_INFINITY, 0.0), ApproxVec::new(0.0, f32::NEG_INFINITY, 0.0));
+				assert_ulps_eq!(ApproxVec::new(0.0, 0.0, f32::INFINITY), ApproxVec::new(0.0, 0.0, f32::MAX));
+				assert_ulps_eq!(ApproxVec::new(f32::NEG_INFINITY, 0.0, 0.0), ApproxVec::new(-f32::MAX, 0.0, 0.0));
+
+				assert_ulps_ne!(ApproxVec::new(0.0, f32::NEG_INFINITY, 0.0), ApproxVec::new(0.0, f32::INFINITY, 0.0));
+			}
 		}
 	}
 }
